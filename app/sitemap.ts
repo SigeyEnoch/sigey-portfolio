717c10eec22dfe7bd3dd7f7df87468a8f48a8aea
Sigey Kunpage Enoch <sigeyenoch05@gmail.com>
2026-08-15 19:45:22 +0000

    Add SEO configuration

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://YOUR-DOMAIN.com",
+      url: "https://sigey-portfolio.vercel.app",
       lastModified: new Date(),
     },
   ];
